830d31abc6da17f570509042520c3d5109a0ae17
Javier Adriel <reivaj0705@gmail.com>
2016-03-12 14:35:40 -0600

    Add trailing slash to the url

--- app/accounts/services/accounts.service.js
+++ app/accounts/services/accounts.service.js
@@ -4,42 +4,9 @@
         .module("app.accounts.services")
         .factory("Accounts", Accounts);
 
-    // Users.$inject = ['$http'];
+    Accounts.$inject = ["$resource"];
+
     function Accounts(){   
-        var Accounts = {
-
-        };
-
-        return Accounts;
-    // function Accounts($http) {
-    //     var Users = {
-    //         getUsers: getUsers,
-    //         getUser: getUser,
-    //         createUser: createUser,
-    //         updateUser: updateUser,
-    //         deleteUser: deleteUser
-    //     }; 
-
-    //     return Users;
-
-    //     function getUsers() {
-    //         return $http.get("http://localhost:8080/all/");
-    //     }
-
-    //     function getUser(id) {
-    //         return $http.get("http://localhost:8080/user/"+id+"/edit/");
-    //     }
-
-    //     function createUser(user) {
-    //         return $http.post("http://localhost:8080/users/create/", user);
-    //     }
-
-    //     function updateUser(user) {
-    //         return $http.put("http://localhost:8080/users/update/", user);
-    //     }
-
-    //     function deleteUser(user) {
-    //         return $http.post("http://localhost:8080/users/delete/", user);
-    //     }
+        return $resource(constants.BASE_URL + "surveys/:surveyId", {surveyId: "@surveyId"});
     }
 })();
