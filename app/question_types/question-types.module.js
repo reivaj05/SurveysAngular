6d53f48875fb603e2a02dcfaf1fc681e78490ea0
Javier Adriel <reivaj0705@gmail.com>
2016-03-16 21:54:30 -0600

    Create question type module

---
+++ app/question_types/question-types.module.js
@@ -0,0 +1,9 @@
+(function () {
+    "use strict";
+
+    angular.module("app.question-types", [
+        "app.question-types.services"
+    ]);
+
+    angular.module("app.question-types.services", []);
+})();
