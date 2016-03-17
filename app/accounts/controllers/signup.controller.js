5731b1631952ada1e853e77f1805976e0520f9c4
Javier Adriel <reivaj0705@gmail.com>
2016-03-16 20:25:22 -0600

    Add signup controller

---
+++ app/accounts/controllers/signup.controller.js
@@ -0,0 +1,10 @@
+(function () {
+    "use strict";
+    angular
+        .module("app.accounts.controllers")
+        .controller("SignupController", SignupController);
+
+    function SignupController() {
+        let self = this;
+    }
+})();
